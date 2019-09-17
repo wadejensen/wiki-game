c3f7fbbf77ad2c8da8672d70c082e24f60deaa09
Wade Jensen <hello@wade-jensen.com>
2019-09-18 00:45:38 +1000

    multiple subscribers

--- app/server/src/main/ts/index.ts
+++ app/server/src/main/ts/index.ts
@@ -2,16 +2,33 @@ import * as Cheerio from "cheerio";
 import {RateLimitedHTTPClient} from "./http/rate_limited_http_client";
 import {FetchHTTPClient} from "./http/fetch_http_client";
 import {LosslessThrottle} from "./lossless_throttle";
-import {Observable, Observer, Subject} from "rxjs";
+import {ConnectableObservable, Observable, Observer, Subject} from "rxjs";
 import {HTTPClient} from "./http/http_client";
 import fs from "fs";
 import {sys} from "typescript";
 import {URL} from "url";
-import {bufferTime, count, distinct, map, scan, tap, windowCount, windowTime} from 'rxjs/operators';
+import {
+  bufferTime,
+  count,
+  distinct,
+  map,
+  multicast, publish,
+  scan,
+  tap,
+  windowCount,
+  windowTime
+} from 'rxjs/operators';
 import {SubjectSubscriber} from "rxjs/internal/Subject";
 import {url} from "inspector";
 
+Array.prototype.flatMap = function(lambda) {
+  return Array.prototype.concat.apply([], this.map(lambda));
+};
+
 start();
+
+// do not get carried away crawling
+setTimeout(() => sys.exit(0), 20000);
 
 async function start() {
   const argv = require('yargs').argv;
@@ -20,7 +37,7 @@ async function start() {
 
   const httpClient = new RateLimitedHTTPClient(
     new FetchHTTPClient(3000, 3, 300, true),
-    new LosslessThrottle(2),
+    new LosslessThrottle(1),
   );
 
   const seedFilePath = argv["seed-file"];
@@ -34,17 +51,25 @@ async function start() {
     .map(line => line.trim());
 
   console.log(seedUrls);
-  const crawler = new Subject<string>()
-
-  const sub1 = crawler
-    .pipe(
-      distinct(),
-      map(url => 1),
-      scan(count => count + 1),
-    ).subscribe((res: number) => console.log(res));
+  const crawlerSubject = new Subject<string>();
+  const distinctUrls = crawlerSubject.pipe(
+    distinct(),
+    multicast(() => new Subject<string>())
+  ) as ConnectableObservable<string>;
 
 
-  seedUrls.map(url => crawl(crawler, httpClient, url));
+  seedUrls.map(url => crawl(crawlerSubject, httpClient, url));
+
+  const counter = distinctUrls
+  .pipe(
+    map(url => 1),
+    scan(count => count + 1),
+  ).subscribe((count: number) => console.log(count));
+
+  const printer = distinctUrls
+    .subscribe((url: string) => console.log(url));
+
+  distinctUrls.connect();
 }
 
 async function crawl(subject: Subject<string>, httpClient: HTTPClient, url: string): Promise<void> {
@@ -56,7 +81,7 @@ async function crawl(subject: Subject<string>, httpClient: HTTPClient, url: stri
     const document = Cheerio.load(html);
     // scrape page for href links
     const hrefs = document("[href]").map((i,tag) => tag.attribs["href"]).get();
-    const links = hrefs.map(href => normaliseHref(href, url));
+    const links = hrefs.flatMap(href => normaliseHref(href, url));
     links.forEach((val, i) => subject.next(val));
     links.forEach(link => crawl(subject, httpClient, link));
   } catch (err) {
@@ -65,15 +90,19 @@ async function crawl(subject: Subject<string>, httpClient: HTTPClient, url: stri
   }
 }
 
-function normaliseHref(href: string, sourceUrl: string): string {
+function normaliseHref(href: string, sourceUrl: string): string[] {
   // Include only with "http" and "https" schemes
   if (href.startsWith("http://") || href.startsWith("https://")) {
-    return href;
-  } else {
+    return [href];
+  } else if (href.match("[-a-zA-Z0-9]+://.*")) {
+    // ignore unwanted url schemes
+    return [];
+  }
+  else {
     const url = new URL(href, sourceUrl);
     // Ignore page fragment subsection link
     url.hash = "";
-    return url.href
+    return [url.href];
   }
 }
 
